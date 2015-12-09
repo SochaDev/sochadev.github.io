6ac126292ca4193aba3ec70c3842bf43727f70d4
Nate Mow <natemow@gmail.com>
2015-12-08 19:27:05 -0500

    Changed from ChromeDriver to standalone Selenium.

--- tests/intern/intern.js
+++ tests/intern/intern.js
@@ -1,19 +1,29 @@
 define({
 
   capabilities: {
-
   },
   environments: [
-    { browserName: 'chrome' },
+    { browserName: 'chrome', platform: ['LINUX'] },
+    { browserName: 'firefox', platform: ['LINUX'] }
+    /*
+    {browserName: 'internet explorer', version: '11', platform: 'WIN8'},
+    {browserName: 'internet explorer', version: '10', platform: 'WIN8'},
+    {browserName: 'internet explorer', version: '9', platform: 'WINDOWS'},
+    {browserName: 'firefox', version: '37', platform: ['WINDOWS', 'MAC']},
+    {browserName: 'chrome', version: '39', platform: ['WINDOWS', 'MAC']},
+    {browserName: 'safari', version: '8', platform: 'MAC'}
+    */
   ],
   tunnel: 'NullTunnel',
-  maxConcurrency: 2,
+  tunnelOptions: {
 
+  },
+  maxConcurrency: 2,
   loaderOptions: {
-    packages: [ ]
+    packages: []
   },
   // Non-functional test suite(s) to run in each browser
-  suites: [ ],
+  suites: [],
   // Functional test suite(s) to execute against each browser once non-functional tests are completed
   functionalSuites: [
     'tests/intern/functional/index'
